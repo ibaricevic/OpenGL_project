ea8ca4f361518989d32026dd499e834162faf753
Ivan Baricevic <ibaricevic01@gmail.com>
2025-11-03 21:28:50 +0100

    Add zdk1 version1.

--- res/shaders/fShader.glsl
+++ res/shaders/fShader.glsl
@@ -1,13 +1,23 @@
 #version 330 core
 
-in vec3 normals;
+in vec3 normal;
 in vec2 TexCord;
 
 out vec4 fColor;
 
-uniform sampler2D tex;
+uniform sampler2D tex;    // texture sampler (0)
+uniform vec3 uColor;      // boja definirana u C++ (r,g,b)
+uniform float useTexture; // 1.0 = koristi teksturu, 0.0 = samo boja
 
 void main()
 {
-	fColor=texture(tex, TexCord);
+    // dohvat boje teksture
+    vec4 tcol = texture(tex, TexCord);
+
+    // Ako je useTexture==1 koristimo teksturu i tintamo ju s uColor
+    // Ako je useTexture==0, ispisujemo samo uColor
+    if (useTexture >= 0.5)
+        fColor = vec4(uColor, 1.0) * tcol;
+    else
+        fColor = vec4(uColor, 1.0);
 }
--- res/shaders/vShader.glsl
+++ res/shaders/vShader.glsl
@@ -6,9 +6,15 @@ layout (location = 2) in vec2 aTexCord;
 out vec3 normal;
 out vec2 TexCord;
 
+// uniform za pomak, koristimo vec3 tako da možemo lako dodati i Z ako zatreba
+uniform vec3 offset;
+
 void main()
 { 
-	gl_Position=vec4(aPos,1.0f);
-	normal=aNorm;
-	TexCord=aTexCord;
+    // pomaknemo vertex poziciju za offset (offset.xy = horizontalni i vertikalni pomak)
+    vec3 movedPos = aPos + offset;
+	gl_Position = vec4(movedPos, 1.0f);
+
+	normal = aNorm;
+	TexCord = aTexCord;
 }
