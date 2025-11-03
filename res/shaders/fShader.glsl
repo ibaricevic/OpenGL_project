2eae7efbd4dd7c5e415f97c35984570ee108be05
Ivan Baricevic <ibaricevic01@gmail.com>
2025-11-03 22:28:38 +0100

    Add v2 of lab3.

--- res/shaders/fShader.glsl
+++ res/shaders/fShader.glsl
@@ -1,23 +1,14 @@
 #version 330 core
 
-in vec3 normal;
-in vec2 TexCord;
+in vec2 TexCoord;
 
-out vec4 fColor;
+out vec4 FragColor;
 
-uniform sampler2D tex;    // texture sampler (0)
-uniform vec3 uColor;      // boja definirana u C++ (r,g,b)
-uniform float useTexture; // 1.0 = koristi teksturu, 0.0 = samo boja
+uniform vec3 color;
+uniform sampler2D tex;
 
 void main()
 {
-    // dohvat boje teksture
-    vec4 tcol = texture(tex, TexCord);
-
-    // Ako je useTexture==1 koristimo teksturu i tintamo ju s uColor
-    // Ako je useTexture==0, ispisujemo samo uColor
-    if (useTexture >= 0.5)
-        fColor = vec4(uColor, 1.0) * tcol;
-    else
-        fColor = vec4(uColor, 1.0);
+	FragColor = vec4(color, 1.0);
+	FragColor = texture(tex, TexCoord);
 }
