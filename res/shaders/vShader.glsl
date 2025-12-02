0143c88fc85c00c46675196c9cc50f3743698bb5
Ivan Baricevic <ibaricevic01@gmail.com>
2025-12-02 01:26:22 +0100

    Add vj4

--- res/shaders/vShader.glsl
+++ res/shaders/vShader.glsl
@@ -1,15 +1,21 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 2) in vec2 aTexCoord;
+layout (location = 1) in vec3 aNorm;
+layout (location = 2) in vec2 aTexCord;
+
+out vec3 normal;
+out vec2 TexCord;
+
+uniform vec3 offset;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec2 TexCoord;
-
 void main()
 { 
-	gl_Position = projection * view * model * vec4(aPos, 1.0);
-	TexCoord = aTexCoord;
+	gl_Position = projection * view * model * vec4(aPos, 1.0f);
+
+	normal=aNorm;
+	TexCord=aTexCord;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,26 @@
 #include "Shader.h"
 #include "Texture.h"
 
-#include "glm/glm.hpp"
-#include "glm/gtc/type_ptr.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include "../src/vendor/glm/glm.hpp"
+#include "../src/vendor/glm/gtc/type_ptr.hpp"
+#include "../src/vendor/glm/gtc/matrix_transform.hpp"
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-bool usePerspective = true;
+// 3D transformations
+glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
+// glm::mat4 projection = glm::ortho(-5.0f, 5.0f, -7.0f, 7.0f, 0.1f, 100.0f);
+// glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
+std::vector<glm::vec3> positions = {
+       {-1.5f, 0.8f, 0.3f},
+       { 0.0f, 0.0f, 0.0f},
+       { 1.0f, -1.2f, -1.3f}
+};
+
 
 int main()
 {
@@ -26,13 +38,10 @@ int main()
 
     Renderer render;
 
-    std::vector<glm::vec3> positions = {
-        {-1.5f, 0.0f, 0.0f},
-        { 0.0f, 0.0f, 0.0f},
-        { 1.5f, 0.0f, 0.0f}
-    };
+    float offsetX = 0.0f;
+    float offsetY = 0.0f;
 
-    //glm::vec3 color(0.0f, 0.0f, 0.0f);
+    glm::vec3 color(1.0f, 1.0f, 1.0f);
 
     while (!window.isClosed())
     {
@@ -40,61 +49,23 @@ int main()
         render.Clear();
 
         shader.Bind();
+        shader.SetUniformVec3("offset", offsetX, offsetY, 0.0f);
+        shader.SetUniformVec3("color", color);
 
-        // ----------------------------------------------------
-        // VIEW MATRIX – obična statična kamera
-        // ----------------------------------------------------
-        glm::mat4 view = glm::lookAt(
-            glm::vec3(4.0f, 3.0f, 4.0f),   // kamera
-            glm::vec3(0.0f, 0.0f, 0.0f),   // gleda u centar
-            glm::vec3(0.0f, 0.0f, -1.0f)    // up os
-        );
-        shader.SetUniform4x4("view", view);
 
-        // ----------------------------------------------------
-        // PROJECTION MATRIX – P ili O tipka
-        // ----------------------------------------------------
-        GLFWwindow* win = window.getWindow();
-        if (glfwGetKey(win, GLFW_KEY_P) == GLFW_PRESS) usePerspective = true;
-        if (glfwGetKey(win, GLFW_KEY_O) == GLFW_PRESS) usePerspective = false;
-
-        glm::mat4 projection;
-
-        if (usePerspective)
+        for (unsigned int i = 0; i < 3; i++)
         {
-            projection = glm::perspective(
-                glm::radians(45.0f),
-                (float)SCR_WIDTH / (float)SCR_HEIGHT,
-                0.1f, 100.0f
-            );
-        }
-        else
-        {
-            projection = glm::ortho(
-                -4.0f, 4.0f,
-                -3.0f, 3.0f,
-                0.1f, 100.0f
-            );
-        }
+            glm::mat4 mat_model = glm::mat4(1.0f);
+            mat_model = glm::translate(mat_model, positions[i]);
+            //float angle = 20.0f * i;
+            float angle = glfwGetTime() * (i+5);
+            mat_model = glm::rotate(mat_model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shader.SetUniform4x4("model", mat_model);
+            shader.SetUniform4x4("view", view);
+            shader.SetUniform4x4("projection", projection);
 
-        shader.SetUniform4x4("projection", projection);
 
-        // ----------------------------------------------------
-        // RENDER Ukoliko želimo više instanci istog modela
-        // ----------------------------------------------------
-        for (int i = 0; i < positions.size(); i++)
-        {
-            glm::mat4 modelMat = glm::mat4(1.0f);
-
-            // translacija
-            modelMat = glm::translate(modelMat, positions[i]);
-
-            // rotacija
-            float angle = glfwGetTime() * (i + 1);
-            modelMat = glm::rotate(modelMat, angle, glm::vec3(0, 1, 0));
-
-            shader.SetUniform4x4("model", modelMat);
-
+            // glDrawArrays(GL_TRIANGLES, 0, 36);
             model.Draw(shader, tex);
         }
 
